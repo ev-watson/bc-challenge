double softmax fixed

data_init.py
@@ -37,14 +37,14 @@ class BCDataModule(LightningDataModule):
         self.labels = np.load(config.LABELS_PATH, mmap_mode='r')
         n = len(self.data)
 
-        self.train_data = self.data[:int(config.SPLIT * n)]
-        self.train_labels = self.labels[:int(config.SPLIT * n)]
+        self.train_data = self.data[:int(config.TRAIN_SPLIT * n)]
+        self.train_labels = self.labels[:int(config.TRAIN_SPLIT * n)]
 
-        self.val_data = self.data[int(config.SPLIT * n):int(2 * config.SPLIT * n)]
-        self.val_labels = self.labels[int(config.SPLIT * n):int(2 * config.SPLIT * n)]
+        self.val_data = self.data[int(config.TRAIN_SPLIT * n):int(config.VAL_SPLIT * n)]
+        self.val_labels = self.labels[int(config.TRAIN_SPLIT * n):int(config.VAL_SPLIT * n)]
 
-        self.test_data = self.data[int(2 * config.SPLIT * n):]
-        self.test_labels = self.labels[int(2 * config.SPLIT * n):]
+        self.test_data = self.data[int(config.VAL_SPLIT * n):]
+        self.test_labels = self.labels[int(config.VAL_SPLIT * n):]
 
         self.train_dataset = BCDataset(self.train_data, self.train_labels)
         self.val_dataset = BCDataset(self.val_data, self.val_labels)

eval.py
@@ -10,14 +10,16 @@ model = BinaryClassifier()
 model.load_state_dict(torch.load(config.MODEL_PATH))
 model.eval()
 
-print("GETTING ATTEMPT...")
 s = Server()
-s.get()
-binary_input = torch.tensor(np.frombuffer(s.binary, dtype=np.uint8), dtype=torch.float32)
 
-print("MAKING GUESS...")
-guess = model(binary_input.unsqueeze(0)).argmax(dim=1).item()
-print(f"Model guess: {config.ARCHITECTURES[guess]}")
-s.post(config.ARCHITECTURES[guess])
+for _ in range(10):
+    print("GETTING ATTEMPT...")
+    s.get()
+    binary_input = torch.tensor(np.frombuffer(s.binary, dtype=np.uint8), dtype=torch.float32) / 255.0
 
-s.log.info("Guess:[{: >9}]   Answer:[{: >9}]   Wins:[{: >3}]".format(config.ARCHITECTURES[guess], s.ans, s.wins))
+    print("MAKING GUESS...")
+    guess = model(binary_input.unsqueeze(0)).argmax(dim=1).item()
+    print(f"Model guess: {config.ARCHITECTURES[guess]}")
+    s.post(config.ARCHITECTURES[guess])
+
+    s.log.info("Guess:[{: >9}]   Answer:[{: >9}]   Wins:[{: >3}]".format(config.ARCHITECTURES[guess], s.ans, s.wins))

model.py
@@ -21,7 +21,7 @@ class BinaryClassifier(LightningModule):
         x = F.relu(x)
         for layer in self.hidden_layers:
             x = F.relu(layer(x))
-        return F.softmax(self.output_layer(x), dim=1)
+        return self.output_layer(x)
 
     def training_step(self, batch, batch_idx):
         x, y = batch
@@ -35,6 +35,13 @@ class BinaryClassifier(LightningModule):
         y_hat = self(x)
         loss = F.cross_entropy(y_hat, y)
         self.log("val_loss", loss, on_epoch=True, on_step=False, prog_bar=True, logger=True)
+        return loss
+    
+    def test_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(x)
+        loss = F.cross_entropy(y_hat, y)
+        self.log("test_loss", loss, on_epoch=True, on_step=False, prog_bar=True, logger=True)
         return loss
     
     def configure_optimizers(self):